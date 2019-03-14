cb536850f85371d8b5d9245d51805142b0e03515
Giacomo Benati <giacomobenati@mailbox.org>
2019-03-14 22:57:49 +0100

    PrisonerAdminForm now getting the right array of prisoners

--- src/views/PrisonAdminForm.jsx
+++ src/views/PrisonAdminForm.jsx
@@ -3,12 +3,18 @@ import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 import PT from 'prop-types';
 
-import { selectPrisonerId, addPrisoner, editPrisoner, deletePrisoner } from '../states/actionCreators'
+import { selectPrisonerId, addPrisoner, editPrisoner, deletePrisoner, getPrisonersByPrisonId } from '../states/actionCreators'
 
 export class PrisonAdminForm extends React.Component {
     nameRef = React.createRef();
     skillRef = React.createRef(); 
     idNumberRef = React.createRef();
+
+    componentDidUpdate() {
+        if (this.props.authedPrison && !this.props.prisoners) {
+            this.props.getPrisonersByPrisonId(this.props.authedPrison.id)
+        }
+    }
 
     onAddPrisoner(event) {
         event.preventDefault();
@@ -54,27 +60,32 @@ export class PrisonAdminForm extends React.Component {
     }
 
     render() {
+        if(this.props.prisoners !== null) {
+            return (
+                <form>
+                    <select 
+                        name="prisoners" 
+                        value={this.props.selectedPrisonerId} 
+                        onChange={e=> this.props.selectPrisonerId(e.target.value)}
+                    >
+                        <option value="" >Choose a prisoner</option>
+                        {
+                            this.props.prisoners.map(prisoner => 
+                                <option key={prisoner.id} value={prisoner.id}>{prisoner.name}</option>   
+                            )
+                        }
+                    </select>
+                    <input type="text" placeholder="name" ref={this.nameRef}/>
+                    <input type="text" placeholder="skills" ref={this.skillRef}/>
+                    <input type="number" placeholder="id number" ref={this.idNumberRef}/>
+                    <button onClick={e => this.onAddPrisoner(e)}>Add Prisoner</button>
+                    <button onClick={e => this.onEditPrisoner(e)}>Edit Prisoner</button>
+                    <button onClick={e => this.onDeletePrisoner(e)}>Delete Prisoner</button>
+                </form>
+            )
+        }
         return (
-            <form>
-                <select 
-                    name="prisoners" 
-                    value={this.props.selectedPrisonerId} 
-                    onChange={e=> this.props.selectPrisonerId(e.target.value)}
-                >
-                    <option value="" >Choose a prisoner</option>
-                    {
-                        this.props.prisoners.map(prisoner => 
-                            <option key={prisoner.id} value={prisoner.id}>{prisoner.name}</option>   
-                        )
-                    }
-                </select>
-                <input type="text" placeholder="name" ref={this.nameRef}/>
-                <input type="text" placeholder="skills" ref={this.skillRef}/>
-                <input type="number" placeholder="id number" ref={this.idNumberRef}/>
-                <button onClick={e => this.onAddPrisoner(e)}>Add Prisoner</button>
-                <button onClick={e => this.onEditPrisoner(e)}>Edit Prisoner</button>
-                <button onClick={e => this.onDeletePrisoner(e)}>Delete Prisoner</button>
-            </form>
+            <div></div>
         )
     }
 }
@@ -95,6 +106,7 @@ function mapStateToProps(state) {
     return {
         prisoners: state.prisoners,
         selectedPrisonerId: state.selectedPrisonerId,
+        authedPrison: state.authedPrison,
     }
 }
   
@@ -103,7 +115,8 @@ function mapDispatchToProps(dispatch) {
         selectPrisonerId,
         addPrisoner,
         editPrisoner,
-        deletePrisoner
+        deletePrisoner,
+        getPrisonersByPrisonId
     }, dispatch);
 }
   
