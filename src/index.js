e31f02675d7978db5f23f846919b71e7d0acb70c
Alec <alec.jordan.cs@gmail.com>
2019-03-14 11:49:36 -0400

    remove redux dev tools

--- src/index.js
+++ src/index.js
@@ -20,9 +20,7 @@ const store = createStore(
     rootReducer,
     {},
     compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    ),
+      applyMiddleware(thunk))
   );
 
 ReactDOM.render(
